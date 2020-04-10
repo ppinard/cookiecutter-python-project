c983766c4f348d810504f835952fba7e81cd5de0
Philippe Pinard <philippe.pinard@gmail.com>
2020-04-10 12:32:29 +0100

    Fix requirement. Add dummy test.

---
+++ {{ cookiecutter.project_name }}/tests/test_{{ cookiecutter.project_name }}.py
@@ -0,0 +1,14 @@
+""""""
+
+# Standard library modules.
+
+# Third party modules.
+
+# Local modules.
+import {{ cookiecutter.project_name }}
+
+# Globals and constants variables.
+
+def test_import():
+    pass
+    
